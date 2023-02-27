2452a0eca8661dd09b15fa974daf7c75ef918504
Olivia Moreau <moreauolivia49@gmail.com>
2023-02-27 14:04:27 +0100

    setup project

---
+++ start.js
@@ -0,0 +1,42 @@
+const { spawn } = require("child_process");
+const { createInterface } = require("readline");
+
+const vite = spawn("npm", ["run", "start"]);
+let electron;
+vite.stdout.on("data", (data) => {
+    const str = data.toString();
+    if (str.includes("webpack compiled successfully")) {
+        relaunchElectron();
+    }
+});
+
+vite.stdout.pipe(process.stdout);
+vite.stderr.pipe(process.stderr);
+
+const relaunchElectron = () => {
+    if (electron) {
+        if (process.platform === "win32") {
+            spawn("taskkill", ["/pid", electron.pid, "/f", "/t"]);
+        }
+        else {
+            electron.kill("SIGINT");
+        }
+
+    }
+    electron = spawn("npm", ["run", "electron:start"]);
+    electron.stdout.pipe(process.stdout);
+    electron.stderr.pipe(process.stderr);
+}
+
+let rl = createInterface(process.stdin, process.stdout);
+
+const reloadLoop = () => {
+    const answer = rl.question("", (answer) => {
+        if (answer === "rl") {
+            relaunchElectron();
+        }
+        reloadLoop();
+    });
+};
+
+reloadLoop();
